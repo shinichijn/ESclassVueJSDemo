58fb21a39d5c7f6e0337b1749fec8586b4de8054
shinichijn <shinichi_jnie@hotmail.com>
2021-04-29 19:28:53 -0700

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+   <h1> {{test}} </h1>
+  </div>
+</template>
+
+<script>
+
+import Meal from './Meal.js';
+
+const test = new Meal('We got a hambuger!!');
+
+export default {
+  name: "HelloWorld",
+  data: () => ({
+    test: test.eat()
+  }),
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Meal.js
@@ -0,0 +1,9 @@
+export default class Meal {
+  constructor(food) {
+    this.food = food;
+  }
+
+  eat() {
+    return this.food
+  }
+}
